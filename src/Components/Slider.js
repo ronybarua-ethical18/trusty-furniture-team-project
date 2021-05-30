3e13b110a47622904325e5bc3c2bdc62f56f13cb
Omar Faruk Shakil <77062122+OMAR-FARUK-SHAKIL@users.noreply.github.com>
2021-05-30 23:48:11 +0600

    Add files via upload

---
+++ src/Components/Slider.js
@@ -0,0 +1,56 @@
+import React from "react";
+import Carousel from "react-bootstrap/Carousel";
+import img1 from "../images/1.jpg";
+import img2 from "../images/2.jpg";
+import img3 from "../images/3.jpg";
+import img4 from "../images/4.jpg";
+import img5 from "../images/5.jpg";
+
+const Slider = () => {
+  return (
+    <div>
+      <Carousel fade="true" interval="5000" pause="false">
+        <Carousel.Item interval={3000}>
+          <img
+            className="d-block w-100"
+            src={img1}
+            alt="First slide"
+            style={{ height: "100vh" }}
+          />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item interval={3000}>
+          <img
+            className="d-block w-100"
+            src={img2}
+            alt="Second slide"
+            style={{ height: "100vh" }}
+          />
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item interval={3000}>
+          <img
+            className="d-block w-100"
+            src={img3}
+            alt="Third slide"
+            style={{ height: "100vh" }}
+          />
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    </div>
+  );
+};
+
+export default Slider;
